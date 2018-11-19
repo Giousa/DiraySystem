6ae685a94eef09026ca8cc086e9c044eeb592d0c
july1992 <uztime@163.com>
2018-11-19 20:20:04 +0800

    vily5

--- app/src/main/java/com/zmm/diary/ui/activity/BaseActivity.java
+++ app/src/main/java/com/zmm/diary/ui/activity/BaseActivity.java
@@ -52,7 +52,7 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
 
         System.out.println("-------------胜楠添加的");
         System.out.println("-------------胜楠又添加的");
-        System.out.println("---------d");
+        System.out.println("-----hhhhhh");
 
         mContext = this;
 
