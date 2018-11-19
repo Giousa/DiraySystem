0eb3fc0d797de8cb7393cdc56c5a8ed3dc1eddda
july1992 <1149745884@qq.com>
2018-11-19 19:37:43 +0800

    vily3

--- app/src/main/java/com/zmm/diary/ui/activity/BaseActivity.java
+++ app/src/main/java/com/zmm/diary/ui/activity/BaseActivity.java
@@ -52,6 +52,8 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
 
         System.out.println("-------------胜楠添加的");
         System.out.println("-------------胜楠又添加的");
+        System.out.println("----------hhhhh");
+        
         mContext = this;
 
         mUnbinder = ButterKnife.bind(this);
